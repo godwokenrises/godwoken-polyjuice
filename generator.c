ab3765f531714270c1e7ede97a0d25f7d7e99011
Linfeng Qian <thewawar@gmail.com>
2020-11-22 16:41:41 +0800

    Update

--- generator.c
+++ generator.c
@@ -11,6 +11,7 @@
 #include "ckb_syscalls.h"
 
 #include <evmc/evmc.h>
+#include <evmone/evmone.h>
 
 static char debug_buffer[64 * 1024];
 static void debug_print_data(const char *prefix,
@@ -29,11 +30,37 @@ static void debug_print_int(const char *prefix, int64_t ret) {
   ckb_debug(debug_buffer);
 }
 
+struct evmc_host_context {};
+
+int init_message(struct evmc_message *msg, gw_context_t* ctx) {
+  if (ctx->call_context.call_type == GW_CALL_TYPE_CONSTRUCT) {
+    msg->kind = EVMC_CREATE;
+  } else if (ctx->call_context.call_type == GW_CALL_TYPE_HANDLE_MESSAGE) {
+    msg->kind = EVMC_CALL;
+  } else {
+    return -1;
+  }
+
+}
+
 __attribute__((visibility("default"))) int gw_construct(gw_context_t * ctx) {
   return 0;
 }
 
 /* parse args then call another contract */
-__attribute__((visibility("default"))) int gw_handle_message(gw_context_t * ctx) {
-  return 0;
+__attribute__((visibility("default"))) int gw_handle_message(gw_context_t* ctx) {
+  int ret;
+  struct evmc_vm *vm = evmc_create_evmone();
+  struct evmc_host_interface interface;
+  struct evmc_host_context context;
+  struct evmc_message msg;
+  ret = init_message(&msg, ctx);
+  if (ret != 0) {
+    return ret;
+  }
+
+  uint8_t *code_data = NULL;
+  uint32_t code_size = 0;
+  struct evmc_result res = vm->execute(vm, &interface, &context, EVMC_MAX_REVISION, &msg, code_data, code_size);
+  return (int)res.status_code;
 }
