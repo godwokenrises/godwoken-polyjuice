b3f6537a9e2e967cc81b053e13b186a74d9934b8
Linfeng Qian <thewawar@gmail.com>
2021-01-13 17:04:41 +0800

    Add DELEGATECALL test

--- polyjuice-tests/src/test_cases/mod.rs
+++ polyjuice-tests/src/test_cases/mod.rs
@@ -1,11 +1,11 @@
 pub(crate) mod call_multiple_times;
+pub(crate) mod call_selfdestruct;
 pub(crate) mod contract_call_contract;
 pub(crate) mod contract_create_contract;
 pub(crate) mod erc20;
 pub(crate) mod selfdestruct;
 pub(crate) mod simple_storage;
 
-pub(crate) mod call_selfdestruct;
 pub(crate) mod delegatecall;
 pub(crate) mod get_block_info;
 pub(crate) mod parse_log_event;
