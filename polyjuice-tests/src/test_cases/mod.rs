655cb79b4f685e02df7509a03a16d293a2d843ca
Linfeng Qian <thewawar@gmail.com>
2021-01-17 18:33:26 +0800

    Add account already exists test

--- polyjuice-tests/src/test_cases/mod.rs
+++ polyjuice-tests/src/test_cases/mod.rs
@@ -8,10 +8,10 @@ pub(crate) mod get_block_info;
 pub(crate) mod parse_log_event;
 pub(crate) mod selfdestruct;
 pub(crate) mod simple_storage;
-
-pub(crate) mod pre_compiled_contracts;
 pub(crate) mod simple_transfer;
-//  Special pre-compiled contract to support transfer to any sudt
-pub(crate) mod any_sudt_erc20;
 //  The account polyjuice want to create already created by meta_contract
 pub(crate) mod account_already_exists;
+
+pub(crate) mod pre_compiled_contracts;
+//  Special pre-compiled contract to support transfer to any sudt
+pub(crate) mod any_sudt_erc20;
