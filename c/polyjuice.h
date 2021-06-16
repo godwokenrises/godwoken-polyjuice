c01bc78d2bd676bc22f988a2e240ef4733d9c6ef
Linfeng Qian <thewawar@gmail.com>
2021-06-16 10:14:07 +0800

    Call fallback function when msg.input is empty

--- c/polyjuice.h
+++ c/polyjuice.h
@@ -875,9 +875,7 @@ int execute_in_evmone(gw_context_t* ctx,
                       uint32_t to_id,
                       const uint8_t* code_data,
                       const size_t code_size,
-                      bool transfer_only,
                       struct evmc_result* res) {
-  debug_print_int("transfer_only", transfer_only);
   evmc_address sender = msg->sender;
   evmc_address destination = msg->destination;
   struct evmc_host_context context {ctx, code_data, code_size, from_id, to_id, sender, destination, 0};
@@ -885,25 +883,19 @@ int execute_in_evmone(gw_context_t* ctx,
   struct evmc_host_interface interface = {account_exists, get_storage,    set_storage,    get_balance,
                                           get_code_size,  get_code_hash,  copy_code,      selfdestruct,
                                           call,           get_tx_context, get_block_hash, emit_log};
-  if (!transfer_only) {
     /* Execute the code in EVM */
-    debug_print_int("code size", code_size);
-    debug_print_data("msg.input_data", msg->input_data, msg->input_size);
-    *res = vm->execute(vm, &interface, &context, EVMC_MAX_REVISION, msg, code_data, code_size);
-    if (context.error_code != 0) {
-      debug_print_int("context.error_code", context.error_code);
-      return context.error_code;
-    }
-    if (res->gas_left < 0) {
-      ckb_debug("gas not enough");
-      return EVMC_OUT_OF_GAS;
-    }
-  } else {
-    res->output_data = NULL;
-    res->release = NULL;
-    res->output_size = 0;
-    res->status_code = EVMC_SUCCESS;
+  debug_print_int("code size", code_size);
+  debug_print_data("msg.input_data", msg->input_data, msg->input_size);
+  *res = vm->execute(vm, &interface, &context, EVMC_MAX_REVISION, msg, code_data, code_size);
+  if (context.error_code != 0) {
+    debug_print_int("context.error_code", context.error_code);
+    return context.error_code;
   }
+  if (res->gas_left < 0) {
+    ckb_debug("gas not enough");
+    return EVMC_OUT_OF_GAS;
+  }
+
   return 0;
 }
 
@@ -1078,7 +1070,7 @@ int handle_message(gw_context_t* ctx,
   }
 
   /* NOTE: msg and res are updated */
-  ret = execute_in_evmone(ctx, &msg, parent_from_id, from_id, to_id, code_data, code_size, transfer_only, res);
+  ret = execute_in_evmone(ctx, &msg, parent_from_id, from_id, to_id, code_data, code_size, res);
   if (ret != 0) {
     return ret;
   }
