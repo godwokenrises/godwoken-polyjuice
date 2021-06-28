f06543db6facfe7ccd807a21ea699be05f8d2535
Flouse <flouse.yu@cryptape.com>
2021-06-28 03:33:59 +0000

    test: mock gw_store_data

--- polyjuice-tests/fuzz/mock_godwoken.hpp
+++ polyjuice-tests/fuzz/mock_godwoken.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <evmc/evmc.hpp>
 #include <evmc/mocked_host.hpp>
+#include <common.h>
 
 using namespace std;
 using namespace evmc;
@@ -9,6 +10,7 @@ class MockedGodwoken : public MockedHost {
 public:
   std::unordered_map<uint32_t, bytes32> gw_account;
   unordered_map<bytes32, bytes32> state;
+  unordered_map<bytes32, bytes> code_store;
 
   result call(const evmc_message& msg) noexcept override {
     auto result = MockedHost::call(msg);
@@ -26,6 +28,7 @@ struct fuzz_input {
 
   explicit operator bool() const noexcept { return msg.gas != -1; }
 };
+auto in = fuzz_input{};
 
 // mock_godwoken.set_storage(
 //   bytes32({202, 21, 28, 70, 21, 107, 178, 94, 20, 217, 66, 198, 87, 129, 250, 203, 109, 201, 220, 50, 224, 74, 196, 60, 29, 131, 235, 115, 74, 147, 160, 21}),
@@ -38,6 +41,12 @@ inline ostream& operator<<(ostream& stream, const bytes32& b32) {
   for (size_t i = 0; i < 31; i++)
     stream << (uint16_t)b32.bytes[i] << ", ";
   return stream << (uint16_t)b32.bytes[31] << ']';
+}
+inline ostream& operator<<(ostream& stream, const bytes& bs) {
+  for (auto &&i : bs){
+    stream << (uint16_t)i << ' ';
+  }
+  return stream;
 }
 
 bytes32 uint256_to_bytes32(const uint8_t u8[32]) {
@@ -52,8 +61,30 @@ void print_bytes32(bytes32& b32) {
 
 extern "C" int init();
 extern "C" int gw_update_raw(const uint8_t k[32], const uint8_t v[32]);
+/* store code or script */
+extern "C" int gw_store_data(const uint64_t len, uint8_t *data);
 
-auto in = fuzz_input{};
+int gw_store_data(const uint64_t len, uint8_t *data) {
+  uint8_t script_hash[32];
+  blake2b_hash(script_hash, data, len);
+
+  dbg_print("gw_store_data blake2b_hash: ");
+  dbg_print_h256(script_hash);
+
+  bytes bs((uint8_t*)data, len);
+  // dbg_print("bytes: %bs", bs);
+  // TODO: debug_print
+  cout << "\tbytes: " << bs << endl;
+  cout << "\tdata: ";
+  for (size_t i = 0; i < len; i++) {
+    cout << (uint16_t)*(data + i) << ' ';
+  }
+  
+  in.mock_gw.code_store.insert({uint256_to_bytes32(script_hash), bs});
+
+  return 0;
+}
+
 int gw_update_raw(const uint8_t k[32], const uint8_t v[32]) {
   cout << "\tgw_update_raw..." << endl;
   in.mock_gw.state[uint256_to_bytes32(k)] = uint256_to_bytes32(v);
