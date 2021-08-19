7de1484ea6094ac378c550511f7582c67269c7e2
Flouse <flouse@gmail.com>
2021-08-19 13:50:16 +0000

    test: lock version of godwoken in integration-test

--- devtools/ci/integration-test.sh
+++ devtools/ci/integration-test.sh
@@ -13,15 +13,15 @@ if [ -d "$GODWOKEN_DIR" ]
 then
     echo "godwoken project already exists"
 else
-    git clone --depth=1 https://github.com/nervosnetwork/godwoken.git $GODWOKEN_DIR
+    git clone -b master https://github.com/nervosnetwork/godwoken.git $GODWOKEN_DIR
 fi
 cd $GODWOKEN_DIR
-git pull -r origin master
+git checkout 4e65efde86bb16aafc6d29aee1bf6f586077eaf6
 git submodule update --init --recursive
 
 cd tests-deps/godwoken-scripts/c
-git pull -r origin master
-git submodule update --init --recursive
+# git pull -r origin master
+# git submodule update --init --recursive
 make all-via-docker
 
 cd $PROJECT_ROOT
