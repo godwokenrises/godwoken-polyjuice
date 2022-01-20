1c9ef82bba3ad37073c393afcdd6a0cd938ca3a8
zero.qn <zero.qn@outlook.com>
2022-01-20 11:27:38 +0800

    format code

--- c/sudt_contracts.h
+++ c/sudt_contracts.h
@@ -2,15 +2,13 @@
 #ifndef SUDT_CONTRACTS_H_
 #define SUDT_CONTRACTS_H_
 
-#include "polyjuice_utils.h"
 #include "polyjuice_globals.h"
+#include "polyjuice_utils.h"
 
 #define BALANCE_OF_ANY_SUDT_GAS 150
 #define TRANSFER_TO_ANY_SUDT_GAS 300
 
-
-int balance_of_any_sudt_gas(const uint8_t* input_src,
-                            const size_t input_size,
+int balance_of_any_sudt_gas(const uint8_t* input_src, const size_t input_size,
                             uint64_t* gas) {
   *gas = BALANCE_OF_ANY_SUDT_GAS;
   return 0;
@@ -28,12 +26,9 @@ int balance_of_any_sudt_gas(const uint8_t* input_src,
    =======
      output[0..32] => amount
  */
-int balance_of_any_sudt(gw_context_t* ctx,
-                        const uint8_t* code_data,
-                        const size_t code_size,
-                        bool is_static_call,
-                        const uint8_t* input_src,
-                        const size_t input_size,
+int balance_of_any_sudt(gw_context_t* ctx, const uint8_t* code_data,
+                        const size_t code_size, bool is_static_call,
+                        const uint8_t* input_src, const size_t input_size,
                         uint8_t** output, size_t* output_size) {
   int ret;
   if (input_size != (32 + 32)) {
@@ -47,7 +42,7 @@ int balance_of_any_sudt(gw_context_t* ctx,
   }
 
   // Default return zero balance
-  *output = (uint8_t *)malloc(32);
+  *output = (uint8_t*)malloc(32);
   if (*output == NULL) {
     ckb_debug("malloc failed");
     return FATAL_PRECOMPILED_CONTRACTS;
@@ -61,9 +56,10 @@ int balance_of_any_sudt(gw_context_t* ctx,
       return ERROR_BALANCE_OF_ANY_SUDT;
     }
   }
-  evmc_address address = *((evmc_address *)(input_src + 32 + 12));
+  evmc_address address = *((evmc_address*)(input_src + 32 + 12));
   uint128_t balance;
-  ret = sudt_get_balance(ctx, sudt_id, POLYJUICE_SHORT_ADDR_LEN, address.bytes, &balance);
+  ret = sudt_get_balance(ctx, sudt_id, POLYJUICE_SHORT_ADDR_LEN, address.bytes,
+                         &balance);
   if (ret == GW_ERROR_NOT_FOUND) {
     debug_print_int("sudt account not found", sudt_id);
     return 0;
@@ -79,8 +75,7 @@ int balance_of_any_sudt(gw_context_t* ctx,
   return 0;
 }
 
-int transfer_to_any_sudt_gas(const uint8_t* input_src,
-                             const size_t input_size,
+int transfer_to_any_sudt_gas(const uint8_t* input_src, const size_t input_size,
                              uint64_t* gas) {
   *gas = TRANSFER_TO_ANY_SUDT_GAS;
   return 0;
@@ -90,7 +85,8 @@ int transfer_to_any_sudt_gas(const uint8_t* input_src,
   Transfer `sudt_id` token from `from_id` to `to_id` with `amount` balance.
 
   NOTE: This pre-compiled contract need caller to check permission of `from_id`,
-  currently only `solidity/erc20/SudtERC20Proxy.sol` is allowed to call this contract.
+  currently only `solidity/erc20/SudtERC20Proxy.sol` is allowed to call this
+  contract.
 
    input:
    ======
@@ -101,43 +97,38 @@ int transfer_to_any_sudt_gas(const uint8_t* input_src,
 
    output: []
  */
-int transfer_to_any_sudt(gw_context_t* ctx,
-                         const uint8_t* code_data,
-                         const size_t code_size,
-                         bool is_static_call,
-                         const uint8_t* input_src,
-                         const size_t input_size,
+int transfer_to_any_sudt(gw_context_t* ctx, const uint8_t* code_data,
+                         const size_t code_size, bool is_static_call,
+                         const uint8_t* input_src, const size_t input_size,
                          uint8_t** output, size_t* output_size) {
   /* Contract code hash of `SudtERC20Proxy.ContractCode`
      => 0x43a008ec973b648bd71ad67e6b66f2be8a6fa88e89c7dad046c948b00aa866aa */
-  static const uint8_t sudt_erc20_proxy_contract_code_hash[32] =
-  {
-    0x43, 0xa0, 0x08, 0xec, 0x97, 0x3b, 0x64, 0x8b,
-    0xd7, 0x1a, 0xd6, 0x7e, 0x6b, 0x66, 0xf2, 0xbe,
-    0x8a, 0x6f, 0xa8, 0x8e, 0x89, 0xc7, 0xda, 0xd0,
-    0x46, 0xc9, 0x48, 0xb0, 0x0a, 0xa8, 0x66, 0xaa,
+  static const uint8_t sudt_erc20_proxy_contract_code_hash[32] = {
+      0x43, 0xa0, 0x08, 0xec, 0x97, 0x3b, 0x64, 0x8b, 0xd7, 0x1a, 0xd6,
+      0x7e, 0x6b, 0x66, 0xf2, 0xbe, 0x8a, 0x6f, 0xa8, 0x8e, 0x89, 0xc7,
+      0xda, 0xd0, 0x46, 0xc9, 0x48, 0xb0, 0x0a, 0xa8, 0x66, 0xaa,
   };
   /* Contract code hash of `SudtERC20Proxy_UserDefinedDecimals.ContractCode`
      => 0xa816b946a890cd593f780e8b6859a9b82314c5df4c8270d66f7c502e818345dc */
-  static const uint8_t sudt_erc20_proxy_user_defined_decimals_contract_code_hash[32] =
-  {
-    0xa8, 0x16, 0xb9, 0x46, 0xa8, 0x90, 0xcd, 0x59,
-    0x3f, 0x78, 0x0e, 0x8b, 0x68, 0x59, 0xa9, 0xb8,
-    0x23, 0x14, 0xc5, 0xdf, 0x4c, 0x82, 0x70, 0xd6,
-    0x6f, 0x7c, 0x50, 0x2e, 0x81, 0x83, 0x45, 0xdc
-  };
+  static const uint8_t
+      sudt_erc20_proxy_user_defined_decimals_contract_code_hash[32] = {
+          0xa8, 0x16, 0xb9, 0x46, 0xa8, 0x90, 0xcd, 0x59, 0x3f, 0x78, 0x0e,
+          0x8b, 0x68, 0x59, 0xa9, 0xb8, 0x23, 0x14, 0xc5, 0xdf, 0x4c, 0x82,
+          0x70, 0xd6, 0x6f, 0x7c, 0x50, 0x2e, 0x81, 0x83, 0x45, 0xdc};
   if (code_data == NULL || code_size == 0) {
     ckb_debug("Invalid caller contract code");
     return ERROR_TRANSFER_TO_ANY_SUDT;
   }
   uint8_t code_hash[32] = {0};
-  blake2b_hash(code_hash, (uint8_t *)code_data, code_size);
+  blake2b_hash(code_hash, (uint8_t*)code_data, code_size);
   if (memcmp(code_hash, sudt_erc20_proxy_contract_code_hash, 32) != 0 &&
-      memcmp(code_hash, sudt_erc20_proxy_user_defined_decimals_contract_code_hash, 32) != 0
-  ) {
+      memcmp(code_hash,
+             sudt_erc20_proxy_user_defined_decimals_contract_code_hash,
+             32) != 0) {
     ckb_debug("The contract is not allowed to call transfer_to_any_sudt");
     debug_print_data("     got code hash", code_hash, 32);
-    debug_print_data("expected code hash", sudt_erc20_proxy_contract_code_hash, 32);
+    debug_print_data("expected code hash", sudt_erc20_proxy_contract_code_hash,
+                     32);
     return ERROR_TRANSFER_TO_ANY_SUDT;
   }
 
@@ -161,9 +152,10 @@ int transfer_to_any_sudt(gw_context_t* ctx,
     return ERROR_TRANSFER_TO_ANY_SUDT;
   }
 
-  evmc_address from_address = *((evmc_address *)(input_src + 32 + 12));
-  evmc_address to_address = *((evmc_address *)(input_src + 64 + 12));
-  ret = sudt_transfer(ctx, sudt_id, POLYJUICE_SHORT_ADDR_LEN, from_address.bytes, to_address.bytes, amount);
+  evmc_address from_address = *((evmc_address*)(input_src + 32 + 12));
+  evmc_address to_address = *((evmc_address*)(input_src + 64 + 12));
+  ret = sudt_transfer(ctx, sudt_id, POLYJUICE_SHORT_ADDR_LEN,
+                      from_address.bytes, to_address.bytes, amount);
   if (ret != 0) {
     ckb_debug("transfer failed");
     if (is_fatal_error(ret)) {
@@ -177,4 +169,4 @@ int transfer_to_any_sudt(gw_context_t* ctx,
   return 0;
 }
 
-#endif  /* #define SUDT_CONTRACTS_H_ */
+#endif /* #define SUDT_CONTRACTS_H_ */
