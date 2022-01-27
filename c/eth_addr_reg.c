6777ce8b5a0983daa5ea0b856feec1c63d7ec3f4
zero.qn <zero.qn@outlook.com>
2022-01-27 19:26:05 +0800

    format code

--- c/eth_addr_reg.c
+++ c/eth_addr_reg.c
@@ -1,14 +1,14 @@
 /**
  * `ETH Address Registry` layer2 contract
- * 
+ *
  * This contract introduces two-ways mappings between `eth_address` and
  * `gw_script_hash`.
- * 
+ *
  *   - `eth_address` is the address of an Ethereum EOA (externally owned account
  *     ) or a Polyjuice contract account.
- * 
+ *
  *   - Godwoken account script hash(a.k.a. `gw_script_hash`) is a key used for
- *     locating the account lock. Godwoken enforces one-to-one mapping between 
+ *     locating the account lock. Godwoken enforces one-to-one mapping between
  *     layer 2 lock script and accountID.
  */
 
@@ -19,7 +19,7 @@
 /* MSG_TYPE */
 #define MSG_QUERY_GW_BY_ETH 0
 #define MSG_QUERY_ETH_BY_GW 1
-#define MSG_SET_MAPPING     2
+#define MSG_SET_MAPPING 2
 
 int handle_fee(gw_context_t *ctx, uint64_t fee) {
   if (ctx == NULL) {
@@ -36,7 +36,7 @@ int handle_fee(gw_context_t *ctx, uint64_t fee) {
 
   const uint32_t CKB_SUDT_ACCOUNT_ID = 1;
   const uint64_t SHORT_SCRIPT_HASH_LEN = DEFAULT_SHORT_SCRIPT_HASH_LEN;
-  return sudt_pay_fee(ctx, CKB_SUDT_ACCOUNT_ID, SHORT_SCRIPT_HASH_LEN, 
+  return sudt_pay_fee(ctx, CKB_SUDT_ACCOUNT_ID, SHORT_SCRIPT_HASH_LEN,
                       payer_account_script_hash, fee);
 }
 
@@ -68,9 +68,8 @@ int main() {
   if (msg.item_id == MSG_QUERY_GW_BY_ETH) {
     mol_seg_t eth_address_seg = MolReader_EthToGw_get_eth_address(&msg.seg);
     uint8_t script_hash[GW_VALUE_BYTES] = {0};
-    ret = load_script_hash_by_eth_address(&ctx,
-                                          eth_address_seg.ptr,
-                                          script_hash);
+    ret =
+        load_script_hash_by_eth_address(&ctx, eth_address_seg.ptr, script_hash);
     if (ret != 0) {
       return ret;
     }
@@ -78,13 +77,11 @@ int main() {
     if (ret != 0) {
       return ret;
     }
-  }
-  else if (msg.item_id == MSG_QUERY_ETH_BY_GW) {
+  } else if (msg.item_id == MSG_QUERY_ETH_BY_GW) {
     mol_seg_t script_hash_seg = MolReader_GwToEth_get_gw_script_hash(&msg.seg);
     uint8_t eth_address[ETH_ADDRESS_LEN] = {0};
-    ret = load_eth_address_by_script_hash(&ctx,
-                                          script_hash_seg.ptr,
-                                          eth_address);
+    ret =
+        load_eth_address_by_script_hash(&ctx, script_hash_seg.ptr, eth_address);
     if (ret != 0) {
       return ret;
     }
@@ -92,8 +89,7 @@ int main() {
     if (ret != 0) {
       return ret;
     }
-  }
-  else if (msg.item_id == MSG_SET_MAPPING) {
+  } else if (msg.item_id == MSG_SET_MAPPING) {
     /* charge fee */
     mol_seg_t fee_seg = MolReader_SetMapping_get_fee(&msg.seg);
     uint64_t fee = *(uint64_t *)fee_seg.ptr;
@@ -102,13 +98,13 @@ int main() {
       return ret;
     }
 
-    mol_seg_t script_hash_seg = MolReader_SetMapping_get_gw_script_hash(&msg.seg);
+    mol_seg_t script_hash_seg =
+        MolReader_SetMapping_get_gw_script_hash(&msg.seg);
     ret = eth_address_register(&ctx, script_hash_seg.ptr);
     if (ret != 0) {
       return ret;
     }
-  }
-  else {
+  } else {
     return GW_FATAL_UNKNOWN_ARGS;
   }
 
